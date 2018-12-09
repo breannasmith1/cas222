3ff08d3fea2af740faa24ff79ec3efa2ccaf9f3f
breannasmith1 <44737367+breannasmith1@users.noreply.github.com>
2018-12-09 11:29:12 -0800

    Delete header.inc.html.php

--- includes/header.inc.html.php
+++
@@ -1,8 +0,0 @@
-<?php define("BASE_URL", "/cas222/aceinthehole/"); ?> 
-
-<header>
-	<img src="<?php echo BASE_URL; ?>images/ace_logo.png" alt="Ace Logo" id="aceLogo">
-    
-        <?php include 'nav.inc.html.php'; ?>
-    
-</header>
